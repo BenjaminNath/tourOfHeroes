5ffe2fc1a08b8262b77d8d1984744e6de804f75f
Ben Nath <ben.nath@dcsg.com>
2018-09-18 14:32:05 -0400

    shell

--- shellScripts/testing_shell.sh
+++ shellScripts/testing_shell.sh
@@ -5,6 +5,6 @@ npm install
 echo "building tour of heroes"
 npm run build:production
 echo "App has finished building. here's the current path"
-cp dist ../compiledHeroes
+cp -R dist/ ../compiledHeroes
 echo "copied files from dist to compiledHeroes"
 ls -a ../compiledHeroes
