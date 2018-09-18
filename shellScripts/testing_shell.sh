0f94e863c71b10db9325e1965ab0420c8f262760
Ben Nath <ben.nath@dcsg.com>
2018-09-18 13:44:10 -0400

    updated shell

--- shellScripts/testing_shell.sh
+++ shellScripts/testing_shell.sh
@@ -4,4 +4,7 @@ cd angular-tour-of-heroes
 npm install
 echo "building tour of heroes"
 npm run build:production
-ls -a
+echo "looking in test-api"
+ls -a test-api/
+echo "looking in compiledHeroes"
+ls -a compiledHeroes/
