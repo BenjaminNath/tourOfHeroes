c85f7cae80d7f6379617b2ef54d74e86e4bcab5d
Ben Nath <ben.nath@dcsg.com>
2018-09-18 14:34:42 -0400

    shell

--- shellScripts/testing_shell.sh
+++ shellScripts/testing_shell.sh
@@ -7,4 +7,4 @@ npm run build:production
 echo "App has finished building. here's the current path"
 cp -R dist/ ../compiledHeroes
 echo "copied files from dist to compiledHeroes"
-ls -a ../compiledHeroes
+ls -a ../compiledHeroes/dist
