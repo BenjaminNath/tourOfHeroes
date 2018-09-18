405424045905f3f9b69d212215eca529bbf9aaa2
Ben Nath <ben.nath@dcsg.com>
2018-09-18 13:49:06 -0400

    shell

--- shellScripts/testing_shell.sh
+++ shellScripts/testing_shell.sh
@@ -4,7 +4,5 @@ cd test-api
 npm install
 echo "building tour of heroes"
 npm run build:production
-echo "looking in test-api"
-ls -a test-api/
-echo "looking in compiledHeroes"
-ls -a compiledHeroes/
+echo "App has finished building."
+ls -a
